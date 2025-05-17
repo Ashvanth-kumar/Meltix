514163ff8b2b3d91e6c6ccacac471f629144d2c0
Ashvanth-kumar <iamashvanthkumar47@gmail.com>
2025-05-17 17:16:17 +0530

    Added functionalities to deploy

--- meltix/src/components/Main.jsx
+++ meltix/src/components/Main.jsx
@@ -1,48 +1,42 @@
 import React from "react"
+import IngredientList from "./IngredientsList"
+import Recipe from "./Recipe"
+// import {getRecipeFromMistral } from ""
 
 export default function Main(){
-    // const ingredients =["asdf","sdfasd","sdfsdfasd"]
+
+
     const [ingredients,setIngredients] = React.useState([])
+    const [recipeShown, setRecipeShown] = React.useState(false)
 
-    const ingredientsList = ingredients.map(ingredient =>(
-        <li key={ingredient}>{ ingredient} </li>
-    ))
+    function toggleRecipeShown(){
+        setRecipeShown(prevShown => !prevShown )
+        console.log({recipeShown})
+    }
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
-        const newIngredient = formData.get("item")
-        // console.log({newIngredient},"added")
+    function addIngredient(formData) {
+
+        const newIngredient = formData.get("ingredient")
         setIngredients(previngredients =>(
             [...previngredients, newIngredient]
         ))
     }
+
     return (
         <main>
             <form 
-                onSubmit={handleSubmit} className="add-ingredient-form">
+                action={addIngredient} className="add-ingredient-form">
                 <input 
                     type="text " 
                     placeholder=" eg.Oregano"
-                    name="item"
+                    name="ingredient"
                     aria-label="Add ingredient"
                 />
                 <button > Add</button>
             </form>
-            {ingredients.length > 0 && <section className="Ingredients-section">
-                <h2>
-                    Ingredients on hand :
-                </h2>
-                <ul className="ingerdients-list" aria-live="polite"> {ingredientsList}  </ul>
-                {ingredients.length > 3  && <div className="get-recipe-container">
-                    <div className="Strts">
-                        <h3>Ready for a recipe ?</h3>
-                        <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <button>Get a recipe</button>
-                </div>}
-            </section>
-            }
+            {ingredients.length > 0 && <IngredientList ingredients={ingredients} toggleRecipeShown = {toggleRecipeShown}/>}
+            {recipeShown && <Recipe />}
         </main>
+
     )
 }
